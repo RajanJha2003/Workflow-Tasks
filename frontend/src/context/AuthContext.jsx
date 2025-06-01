2ff6f9814216045ed1806ead66111404ed9f1bce
RajanJha2003 <rajanj448@student.sfit.ac.in>
2025-06-01 15:34:00 +0530

    10:Frontend auth

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,47 +1,39 @@
+import React, { createContext, useContext, useEffect } from "react";
+import { useState } from "react";
 
+import axios from "axios";
 
+export const authDataContext = createContext();
 
-import axios from 'axios';
-import React from 'react'
-import { useEffect } from 'react';
-import { useState } from 'react';
-import { createContext } from 'react'
+const AuthContext = ({ children }) => {
+  let [userData, setUserData] = useState(null);
+  const getCurrentUser = async () => {
+    try {
+      let result = await axios.get("http://localhost:5000/api/user/current-user", {
+        withCredentials: true,
+      });
+      setUserData(result.data.user);
 
-export const authDatacContext=createContext();
-
-
-
-
-
-const AuthContext = ({children}) => {
-    const [userData,setUserData]=useState(null);
-
-    const getCurrentUser=async()=>{
-       try {
-         const res=await axios.get("http://localhost:5000/api/user/current-user",{withCredentials:true});
-
-        setUserData(res.data.user);
-        console.log(res);
-       } catch (error) {
-        console.error("Error fetching current user:", error);
-        
-       }
-
+      console.log(result);
+    } catch (error) {
+      console.log(error);
+      setUserData(null);
     }
+  };
 
-    useEffect(()=>{
-        getCurrentUser();
-    },[])
+  useEffect(()=>{
+    getCurrentUser();
+  },[])
 
-    const values={
-        userData,
-        setUserData
-    }
+  const values = {
+    userData,
+    setUserData,
+  };
   return (
-    <authDatacContext.Provider value={values}>
-        {children}
-    </authDatacContext.Provider>
-  )
-}
+    <authDataContext.Provider value={values}>
+      {children}
+    </authDataContext.Provider>
+  );
+};
 
-export default AuthContext
+export default AuthContext;
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,55 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useContext, useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom';
+import { authDataContext } from '../context/AuthContext';
 
 const Login = () => {
+
+  const [email,setEmail]=useState('');
+  const [password,setPassword]=useState('');
+
+  const {userData,setUserData}=useContext(authDataContext);
+
+  const navigate=useNavigate();
+
+
+  const handleSubmit=async(e)=>{
+    try {
+      e.preventDefault();
+
+    const res=await axios.post("http://localhost:5000/api/user/login",{
+      email,password
+    },{withCredentials:true})
+
+    console.log(res);
+    setUserData(res.data);
+    navigate("/");
+
+    setEmail('');
+    setPassword('');
+    } catch (error) {
+      console.log(error);
+      
+    }
+
+  }
   return (
-    <div>Login</div>
+    <div className='min-h-screen flex items-center justify-center bg-gray-100'>
+      <form onSubmit={handleSubmit} className='bg-white p-8 rounded-lg shadow-md w-full max-w-md space-y-4 ' >
+        <h2 className='text-2xl font-bold text-center text-blue-600'>Login</h2>
+        <input type="text" placeholder='Email' value={email} onChange={(e)=>setEmail(e.target.value)} className='w-full p-3 border rounded border-gray-300' />
+        <input type="text" placeholder='Password' value={password} onChange={(e)=>setPassword(e.target.value)} className='w-full p-3 border rounded border-gray-300' />
+       <button type='submit' 
+       className='w-full bg-blue-600 text-white py-2 rounded hover:bg-blue-700 transition'
+       >
+        Login
+       </button>
+       <p className='text-sm text-center'>
+        Don&apos;t have an account?
+        <Link className='text-blue-600 hover:underline' to={"/sign-up"}>Sign Up here</Link>
+       </p>
+      </form>
+    </div>
   )
 }
 
